0f11d3a7e8798801ec2ead877aa8a306144b87c1
Agustin Latrelle <luis.latrelle@mercadolibre.com>
2022-05-19 12:07:31 -0300

    add initial app package for apis

---
+++ api/app/app.go
@@ -0,0 +1,35 @@
+//Package app el paquete application contiene todos los métodos y recursos comunes para usar por una api
+package app
+
+import (
+	"github.com/go-chi/jwtauth"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+	"time"
+)
+
+const (
+	LogLevelDB   = 1
+	LogLevelAuth = 2
+	LogLevelHttp = 4
+)
+
+var (
+	//TokenAuth Estructura para crear y validar token JWT
+	TokenAuth *jwtauth.JWTAuth
+	//Exp Tiempo de expiracion del JWT
+	Exp time.Duration
+	//Secret utilizado para la creacion de JWT
+	Secret string
+)
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.SetGlobalLevel(-1)
+	log.Logger = log.With().Caller().Logger()
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	Exp = 600 * time.Minute
+	Secret = os.Getenv("API_JWT_SECRET")
+	TokenAuth = jwtauth.New("HS256", []byte(Secret), nil)
+}
---
+++ go.mod
@@ -0,0 +1,22 @@
+module toolkit-api
+
+go 1.17
+
+require (
+	github.com/go-chi/jwtauth v1.2.0
+	github.com/rs/zerolog v1.26.1
+	github.com/unrolled/render v1.4.1
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/goccy/go-json v0.3.5 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.7 // indirect
+	github.com/lestrrat-go/httpcc v1.0.0 // indirect
+	github.com/lestrrat-go/iter v1.0.0 // indirect
+	github.com/lestrrat-go/jwx v1.1.0 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20211215165025-cf75a172585e // indirect
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+)
